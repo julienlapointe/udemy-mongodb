b114ce2f40da5d7f5777cdfee0582bab96ded56e
Julien LaPointe <email@julienlapointe.com>
2017-08-04 19:41:00 -0500

    installed mongoose, mocha, and nodemon to start writing tests

--- users/test/test_helper.js
+++ users/test/test_helper.js
@@ -1,23 +1,34 @@
+// this file sets up / configures the testing environment
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
 before((done) => {
-  mongoose.connect('mongodb://localhost/users_test');
-  mongoose.connection
-    .once('open', () => { done(); })
-    .on('error', (error) => {
-      console.warn('Warning', error);
-    });
+	// initiate connection between Mongoose and MongoDB
+	// replace "localhost" with "IPaddress:port" in staging / production environments
+	// "users_test" = database name
+	// you don't need to create the database ahead of time (you can simply name the database and add a record to it, then Mongoose/MongoDB will automatically create the database)
+	mongoose.connect('mongodb://localhost/users_test');
+	mongoose.connection
+		// event listener
+		// listen for "open" event to happen once (if "open" occurs multiple times, only fire the callback function on the 1st occurance)
+		// if the Mongoose-MongoDB connection is opened, then...
+		.once('open', () => { done(); })
+		// event listener
+		// listen for the "error" event
+		// otherwise, if Mongoose-MongoDB connection fails, then...
+		.on('error', (error) => {
+			console.warn('Warning', error);
+		});
 });
 
 beforeEach((done) => {
-  const { users, comments, blogposts } = mongoose.connection.collections;
-  users.drop(() => {
-    comments.drop(() => {
-      blogposts.drop(() => {
-        done();
-      });
-    });
-  });
+	const { users, comments, blogposts } = mongoose.connection.collections;
+	users.drop(() => {
+		comments.drop(() => {
+			blogposts.drop(() => {
+				done();
+			});
+		});
+	});
 });
